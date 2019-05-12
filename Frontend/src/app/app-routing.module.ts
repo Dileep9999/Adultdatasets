d26cd1045f1784001430f8ee93d0c4777d3e23da
Dileep <apple@Apples-MacBook-Pro.local>
2019-05-12 15:03:21 +0530

    UI update

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+
+
+const routes: Routes = [
+  // {
+  //   path:'dash',
+  //   component:DashBoardComponent
+  // }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
